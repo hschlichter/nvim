c9200c4e02890f835687c0b677a4f16d93371b82
Henrik Schlichter <henriksc@unity3d.com>
2023-03-14 14:39:36 +0100

    added relative filepath to lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,5 +4,13 @@ require("lualine").setup({
         theme = "onedark",
         component_separators = "|",
         section_separators = "",
+    },
+    sections = {
+        lualine_c = {
+            {
+                "filename",
+                path = 1,
+            }
+        }
     }
 });
